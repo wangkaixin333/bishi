49bb7827819ab007f2c703e308a2948058329bce
wangkai333 <18811048865@163.com>
2021-03-28 19:56:14 +0800

    2

--- kaoshi/aaa.java
+++ kaoshi/aaa.java
@@ -7,6 +7,7 @@ public class aaa {
     public static void main(String[] args) {
         System.out.println("aiyingxin");
         System.out.println("wfg");
+        System.out.println("wfg");
         }
     }
 
