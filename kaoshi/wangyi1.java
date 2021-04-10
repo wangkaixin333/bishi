065161d4741c5578ddb484c965c5e473d6a75b66
wangkai333 <18811048865@163.com>
2021-04-10 16:00:51 +0800

    2

--- kaoshi/wangyi1.java
+++ kaoshi/wangyi1.java
@@ -1,8 +1,16 @@
 public class wangyi1 {
+    public static void main(String[] args) {
+        System.out.println(f(4));
+
+    }
     public  static int f(int n){
-        if( n == 1 || n == 2 )
-            return 1;
-        else
-            return f( n - 1 )+f( n - 2 );
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2]+dp[i-3];
+        }
+        return dp[n];
     }
 }
