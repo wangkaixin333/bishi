f0a335720d23b83f13363acec1f21f4299d2707d
wangkai333 <18811048865@163.com>
2021-04-13 11:55:03 +0800

    Default Changelist

--- kaoshi/learn/sumWays.java
+++ kaoshi/learn/sumWays.java
@@ -3,22 +3,23 @@ package learn;
 import java.util.Scanner;
 
 public class sumWays {
-    public class numWays {
-        public static void main(String[] args) {
-            Scanner scan1 = new Scanner(System.in);
-            int n = scan1.nextInt();
-            System.out.println("请输入一个n值：");
-            Ways(n);
-        }
-        public static int Ways(int n) {
-            int a = 1, b = 1, sum;
-            for (int i = 0; i < n; i++) {
-                sum = (a + b) % 1000000007;
-                a = b;
-                b = sum;
-            }
-            return a;
-        }
+
+    public static void main(String[] args) {
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("请输入一个n值：");
+        int n = scan1.nextInt();
+        System.out.println(Ways(n));
     }
+
+    public static int Ways(int n) {
+        int a = 1, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
 
 }
