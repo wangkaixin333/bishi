9604b037af5c6a88d52f0a8fb535a8a1c634ec7f
wangkaixin333 <18811048865@163.com>
2021-04-28 16:50:56 +0800

    广联达

---
+++ kaoshi/duxiaomankaixin/duxiaoman111.java
@@ -0,0 +1,61 @@
+package duxiaomankaixin;
+
+import java.util.Scanner;
+
+public class duxiaoman111 {
+    public static void main(String[] args) {
+        String cat = "cat";
+        String pig = "pig";
+        String horse = "horse";
+        Scanner read = new Scanner(System.in);
+        String shuru = read.next();
+        if (getSimilarityRatio(shuru, cat) > 0.66) {
+            System.out.println(cat);
+        } else if (getSimilarityRatio(shuru, pig) > 0.66) {
+            System.out.println(pig);
+        } else {
+            System.out.println(horse);
+
+        }
+
+    }
+    public static float getSimilarityRatio(String str, String target) {
+
+        int d[][]; // 矩阵
+        int n = str.length();
+        int m = target.length();
+        int i; // 遍历str的
+        int j; // 遍历target的
+        char ch1; // str的
+        char ch2; // target的
+        int temp; // 记录相同字符,在某个矩阵位置值的增量,不是0就是1
+        if (n == 0 || m == 0) {
+            return 0;
+        }
+        d = new int[n + 1][m + 1];
+        for (i = 0; i <= n; i++) { // 初始化第一列
+            d[i][0] = i;
+        }
+
+        for (j = 0; j <= m; j++) { // 初始化第一行
+            d[0][j] = j;
+        }
+
+        for (i = 1; i <= n; i++) { // 遍历str
+            ch1 = str.charAt(i - 1);
+            // 去匹配target
+            for (j = 1; j <= m; j++) {
+                ch2 = target.charAt(j - 1);
+                if (ch1 == ch2 || ch1 == ch2 + 32 || ch1 + 32 == ch2) {
+                    temp = 0;
+                } else {
+                    temp = 1;
+                }
+                // 左边+1,上边+1, 左上角+temp取最小
+                d[i][j] = Math.min(Math.min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + temp);
+            }
+        }
+
+        return (1 - (float) d[n][m] / Math.max(str.length(), target.length())) * 100F;
+    }
+}
