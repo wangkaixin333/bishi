6e17170b965c3a7001c5877c9b886f76f38758d4
wangkaixin333 <18811048865@163.com>
2021-04-28 17:05:07 +0800

    度小满

--- kaoshi/duxiaomankaixin/duxiaoman2.java
+++ kaoshi/duxiaomankaixin/duxiaoman2.java
@@ -1,7 +1,12 @@
 package duxiaomankaixin;
 
+import java.util.Scanner;
+
 public class duxiaoman2 {
     public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int n = read.nextInt();
+        int m = read.nextInt();
 
     }
 }
