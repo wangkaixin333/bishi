af6c1505e7f4ea8311aef249c6036add26d23ca9
wangkaixin333 <18811048865@163.com>
2021-08-28 21:24:55 +0800

    aiyingxin

--- kaoshi/ali1.java
+++ kaoshi/ali1.java
@@ -4,27 +4,21 @@ public class ali1 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         int t = read.nextInt();
-        for (int i = 0; i < t; i++) {
-            int shu = read.nextInt();
-            jisuan(shu);
-        }
+        System.out.println(reverse(t));
     }
+//aiyingxin
 
-    public static void jisuan(int n) {
-        int j = (int) Math.cbrt(n);
-        if (j * j * j == n) {
-            System.out.println(j + " " + j);
-
-        }
-        else {
-            for (int x = 0; x <= j; x++) {
-                for (int y = x; y <= j; y++) {
-                    if (((y * y + y) / 2) * ((y * y + y) / 2) - (((x * x + x) / 2) * ((x * x + x) / 2)) == n) {
-                        System.out.println(x + 1 + " " + y);
-                    }
-                }
+    public static int reverse(int x) {
+        long sum = 0;
+        while (x != 0) {
+            int y = x % 10;
+            sum = y + sum * 10;
+            if (sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) {
+                return 0;
             }
+            x = x / 10;
         }
+        return (int) sum;
     }
 
 }
