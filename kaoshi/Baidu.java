cffe0721622b02c79efd9b9a9cb43dc7d2afe1e5
wangkaixin333 <18811048865@163.com>
2021-08-29 11:42:30 +0800

    aiyingxin

--- kaoshi/Baidu.java
+++ kaoshi/Baidu.java
@@ -1,5 +1,28 @@
+import niukelianxi.ListNode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Baidu {
+
+    public ListNode ReverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+
+        }
+
+        return pre;
+
+    }
     public static void main(String[] args) {
+
+
+
 
     }
 }
--- kaoshi/meituan/meituan1.java
+++ kaoshi/meituan/meituan1.java
@@ -1,4 +1,41 @@
 package meituan;
 
+import java.util.Scanner;
+
 public class meituan1 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int zushu = read.nextInt();
+        for (int i = 0; i < zushu; i++) {
+            int a = read.nextInt();
+            int b = read.nextInt();
+            jisuan(a, b);
+        }
+
+    }
+
+    public static void jisuan(int a, int b) {
+        if (a == b) {
+            System.out.println(0);
+        }else {
+            int min = Math.min(a, b);
+            int max = Math.max(a, b);
+            int i;
+            for (i = 1; min < max; i++) {
+                min = min + i;
+            }
+            if (min == max) {
+                System.out.println(i);
+            } else {
+                i--;
+                min = min - i;
+                System.out.println(i-1+((max - min) * 2));
+            }
+        }
+
+
+    }
+
+
+
 }
