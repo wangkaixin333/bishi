63d0618916925df693ef7f5814197b86b03c08f9
wangkaixin333 <18811048865@163.com>
2021-09-26 20:54:08 +0800

    58笔试第二题

---
+++ kaoshi/niukelianxi/ceshi.java
@@ -0,0 +1,10 @@
+package niukelianxi;
+
+public class ceshi {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = new String(s1);
+        System.out.println(s1 == s2);
+
+    }
+}
