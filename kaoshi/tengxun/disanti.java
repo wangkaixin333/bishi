fb4b8b4d5da61a07e618f13cf8e7ce71c5f8f7ba
wangkaixin333 <18811048865@163.com>
2021-09-26 21:50:35 +0800

    腾讯笔试第三题

--- kaoshi/tengxun/disanti.java
+++ kaoshi/tengxun/disanti.java
@@ -1,4 +1,25 @@
-package tengxun;
 
-public class disanti {
-}
+def at(a,b):
+        res =a|(a+b)
+        return res
+        def calculate(s):
+        stack = []
+        operate,res="+",0
+        l=len(s)
+        for i in range(l):
+        if s[i].isdigit():
+        res = res * 10 + int(s[i])
+        if s[i] in "+x@" or i==l-1:
+        if operate =="+":
+        stack.append(res)
+        elif operate == "x":
+        stack.append(stack.pop()*res)
+        else:
+        stack.append(at(stack.pop(),res))
+        operate = s[i]
+        res=0
+        return sum(stack)
+
+        s = input()
+        result = calculate(s)
+        print(result)
