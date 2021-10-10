2c13e11ac153e7e07031b7462b9863244669561b
wangkaixin333 <18811048865@163.com>
2021-10-10 11:44:01 +0800

    字节第一题

---
+++ kaoshi/zijie/diyiti.java
@@ -0,0 +1,50 @@
+package zijie;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class diyiti {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        char[] chars = str.toCharArray();
+        char[] shuzichars;
+        char[] zifuchars;
+        String stringBuffer1 = new String();
+        String stringBuffer2 = new String();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'z') {
+                stringBuffer1+= String.valueOf(chars[i]);
+            } else if (chars[i] >= '0' && chars[i] <= '9') {
+                stringBuffer2+= String.valueOf(chars[i]);
+            }
+        }
+
+
+        zifuchars = stringBuffer1.toCharArray();
+        shuzichars = stringBuffer2.toCharArray();
+        Arrays.sort(zifuchars);
+        Arrays.sort(shuzichars);
+
+        String stringBuffer3 = new String();
+        String stringBuffer4 = new String();
+        for (int i = 0,a=0,b=shuzichars.length-1; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'z') {
+                stringBuffer3+=zifuchars[a++];
+            } else if (chars[i] >= '0' && chars[i] <= '9') {
+                stringBuffer3 += shuzichars[b--];
+            } else {
+                stringBuffer3 += "?";
+
+            }
+            
+        }
+        System.out.println(stringBuffer3);
+
+    }
+
+
+
+}
