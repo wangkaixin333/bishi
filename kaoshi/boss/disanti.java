b6a022cf09ec9e8717454e7fcb56421b96d3cee8
wangkaixin333 <18811048865@163.com>
2021-10-16 17:24:41 +0800

    zijie

---
+++ kaoshi/boss/disanti.java
@@ -0,0 +1,18 @@
+package boss;
+
+import java.util.TreeSet;
+
+public class disanti {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> s = new TreeSet<Integer>();
+        for (int num : nums) {
+            s.add(num);
+            if (s.size() > 3) {
+                s.remove(s.first());
+            }
+        }
+        return s.size() == 3 ? s.first() : s.last();
+    }
+}
+
+
